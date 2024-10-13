61fad6a165b89c7f3d9ca9bfa740a5411b6f11ff
danross826-design <danross826@gmail.com>
2024-10-12 23:40:06 -0500

    Created User Controller
    Created User Controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class UserController extends Controller
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    
+    public function index()
+    {
+        return User::all();
+    }
+
+    public function show(User $user)
+    {
+        return $user;
+    }
+
+    public function store(Request $request)
+    {
+        $user = User::create($request->all());
+
+        return response()->json($user, 201);
+    }
+
+    public function update(Request $request, User $user)
+    {
+        $user->update($request->all());
+
+        return response()->json($user, 200);
+    }
+
+    public function delete(User $user)
+    {
+        $user->delete();
+
+        return response()->json(null, 204);
+    }
+
+}
