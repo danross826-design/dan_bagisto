e4a3f266c2ef4405098955a32f507c7363acc187
danross826-design <danross826@gmail.com>
2025-05-16 16:19:52 -0500

    labeling
    labeling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,8 @@ $app = new Illuminate\Foundation\Application(
     realpath(__DIR__.'/../')
 );
 
+//This is bootstrap
+
 /*
 |--------------------------------------------------------------------------
 | Bind Important Interfaces
