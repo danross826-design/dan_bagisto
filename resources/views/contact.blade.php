c899a1cb9d0a43f7dcfd23ab36fefc55f85059f9
danross826-design <danross826@gmail.com>
2025-11-08 06:56:49 -0600

    Using layouts
    Using layouts

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,38 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to Laravel</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            min-height: 100vh;
-            margin: 0;
-            background-color: #f8f8f8;
-        }
-        .container {
-            text-align: center;
-            padding: 20px;
-            border-radius: 8px;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-            background-color: #fff;
-        }
-        h1 {
-            color: #333;
-        }
-        p {
-            color: #666;
-        }
-    </style>
-</head>
-<body>
-    <div class="container">
-        <h1>Hello from the contact page!</h1>
-
-    </div>
-</body>
-</html>
+<x-layout>
+    <x-slot:heading>
+        Contacts
+    </x-slot:heading>
+    <h1>Hello from the contacts page!</h1>
+</x-layout>
