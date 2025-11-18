727fbadafef59f3c17178978573cec2963003b6a
danross826-design <danross826@gmail.com>
2025-11-17 23:46:43 -0600

    Update sample-form.blade.php

--- resources/views/sample-form.blade.php
+++ resources/views/sample-form.blade.php
@@ -28,7 +28,6 @@
 
         <label for="email">Email:</label><br>
         <input type="email" id="email" name="email" value="{{ old('email') }}"><br><br>
-        
 
         <label for="message">Message:</label><br>
         <textarea id="message" name="message">{{ old('message') }}</textarea><br><br>
