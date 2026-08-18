90729b9b7b8c3c27a7559a81bb8d6f17c2e854b0
danross826-design <danross826@gmail.com>
2026-08-17 20:07:15 -0500

    Update TagController.php

---
+++ app/Http/Controllers/TagController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Routing\Controller as BaseController;
+
+class JobController extends Controller
+{
+
+}
