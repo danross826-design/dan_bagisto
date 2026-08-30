54fbbea0f2059a9173dde1c0c566844a95f88ac1
danross826-design <danross826@gmail.com>
2026-08-30 17:26:50 -0500

    Create CheckAge.php

---
+++ app/Http/Middleware/CheckAge.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckAge
+{
+    /**
+     * Handle an incoming request.
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($request->age <= 200) {
+            return redirect('/');
+        }
+
+        return $next($request);
+    }
+}
